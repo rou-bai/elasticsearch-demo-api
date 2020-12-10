5732254362784f8137883cf8f0a87b2c3bbab4b5
rou-bai <2802607350@qq.com>
2020-12-10 20:37:32 +0800

    提交一些api使用例子

---
+++ src/main/java/com/fuli/FuliEsApiApplication.java
@@ -0,0 +1,13 @@
+package com.fuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuliEsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuliEsApiApplication.class, args);
+	}
+
+}
